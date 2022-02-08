c851738349740394f6c8e3e675ccb71ec54a76ea
Nikunj Chawla <nikchawla312@gmail.com>
2022-02-08 13:26:07 -0600

    Added table tennis files to server

--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def main_page():
     user_name = request.args.get("userName", "unknown")
-    return render_template('main.html', user=user_name)
+    return render_template('main.html', user = user_name)
+
+
+@app.route('/table-tennis')
+def table_tennis():
+    return render_template('table_tennis.html')
